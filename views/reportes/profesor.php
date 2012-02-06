9544b3db9ab7786fafc61e148f61989d9084be0e
Jorge Toro <maca@maca-PC>
2012-02-06 09:15:05 -0300

    cambios en profesor.php

--- views/reportes/profesor.php
+++ views/reportes/profesor.php
@@ -26,18 +26,15 @@ foreach ($notas_grupo as $id => $nota) {
             echo '[\'' . $nota->apellido . " " . $nota->nombre . '\', 0],';
         }
     }
-};
-/*for ($sinnotas = count($notas_grupo); $sinnotas < $total_estudiantes_grupo; $sinnotas++) {
-    echo ',[\'' . $sinnotas . '\', 0]';
-}*/
+}
 ?>
                       ]);
 
                       var options = {
                           width: 1400, height: 240,
                           title: 'Ranking del Curso',
-                          hAxis: {showTextEvery: 1, showTextEvery:1,gridlines:{count:10}, slantedText:'True', slantedTextAngle: 90,textStyle:{fontSize:10},viewWindowMode:'maximized'},
-                          vAxis: {showTextEvery: 1,viewindow: {min: 0},format:'#%'},
+                          hAxis: {showTextEvery: 1,gridlines:{count:10}, slantedText:'True', slantedTextAngle: 90,textStyle:{fontSize:10},viewWindowMode:'maximized'},
+                          vAxis: {showTextEvery: 1,viewindow: {min: 0},format:'# %'},
                           pointSize:5
                       };
 
@@ -59,31 +56,31 @@ foreach ($notas_grupo as $id => $nota) {
                 ]);
 
                 var options = {
+                    title:'Promedio Curso',
                     width: 400, height: 240,
                     vAxis: {showTextEvery: 1,viewWindow: {min: 0}}
                 };
 
-                var chart = new google.visualization.ColumnChart(document.getElementById('comparacion_grupo'));
+                var chart = new google.visualization.ColumnChart(document.getElementById('promedio_grupo'));
                 chart.draw(data, options);
             }
+            
         </script>  
+        
         <script type="text/javascript">
             <?php
-                $conj_notas[0]=0;
-                $conj_notas[1]=0;
-                $conj_notas[2]=0;
-                $conj_notas[3]=0;
-                $conj_notas[4]=0;
+                $conj_notas=array(0,0,0,0,0);
+                
                 for($j=0;$j<count($notas_grupo);$j++){
-                    if(($nota->logro>=0 && $nota->logro<=$nota_maxima/5) || $nota->logro==NULL){
+                    if(($notas_grupo[$j]->logro>=0 && $notas_grupo[$j]->logro<=$nota_maxima/5) || $notas_grupo[$j]->logro==NULL){
                         $conj_notas[0]++;
-                    }elseif($nota->logro>$nota_maxima/5 && $nota->logro<=$nota_maxima*2/5){
+                    }elseif($notas_grupo[$j]->logro>$nota_maxima/5 && $notas_grupo[$j]->logro<=$nota_maxima*2/5){
                         $conj_notas[1]++;
-                    }elseif($nota->logro>$nota_maxima*2/5 && $nota->logro<=$nota_maxima*3/5){
+                    }elseif($notas_grupo[$j]->logro>$nota_maxima*2/5 && $notas_grupo[$j]->logro<=$nota_maxima*3/5){
                         $conj_notas[2]++;
-                    }elseif($nota->logro>$nota_maxima*3/5 && $nota->logro<=$nota_maxima*4/5){
+                    }elseif($notas_grupo[$j]->logro>$nota_maxima*3/5 && $notas_grupo[$j]->logro<=$nota_maxima*4/5){
                         $conj_notas[3]++;
-                    }elseif($nota->logro>$nota_maxima*4/5 && $nota->logro<=$nota_maxima){
+                    }elseif($notas_grupo[$j]->logro>$nota_maxima*4/5 && $notas_grupo[$j]->logro<=$nota_maxima){
                         $conj_notas[4]++;
                     }
                 }
@@ -104,66 +101,87 @@ foreach ($notas_grupo as $id => $nota) {
                 ]);
 
                 var options = {
+                    title:'Histograma de Notas',
                     width: 400, height: 240,
-                    vAxis: {showTextEvery: 1,viewWindow: {min: 0}}
+                    vAxis: {title:'Frecuencia',showTextEvery: 1,viewWindow: {min: 0}},
+                    hAxis:{title:'Notas'}
                 };
 
                 var chart = new google.visualization.ColumnChart(document.getElementById('histograma'));
                 chart.draw(data, options);
             }
         </script> 
+        <script type="text/javascript">
+            <?php
+                $ausentes=0;
+                $aprobados=0;
+                $reprobados=0;
+                for($i=0;$i<count($notas_grupo);$i++){
+                    if($notas_grupo[$i]->logro==NUll){
+                        $ausentes++;
+                    }elseif($notas_grupo[$i]->logro>45){
+                        $aprobados++;
+                    }else{
+                        $reprobados++;
+                    }
+                }
+            ?>
+      google.load("visualization", "1", {packages:["corechart"]});
+      google.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'situacion');
+        data.addColumn('number', 'alumnos');
+        data.addRows([
+            <?php
+            echo "['Aprobados',".$aprobados."],['Reprobados',".$reprobados."],['Ausentes',".$ausentes."]";
+            ?>
+        ]);
+
+        var options = {
+          width: 450, height: 300
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('aprobados'));
+        chart.draw(data, options);
+      }
+    </script>
+
+    
     </head>
     <body>
-        <h1>Informe de Gestión para el Docente</h1>
-        <!-- TODO: mostrar la fecha de hoy -->
-        <b>Evaluación: <?php echo $nombre_actividad;?></b>
-        <b>Curso: </b><?php echo $nombre_curso; ?></br>
-        <b>Estudiante: </b><?php echo ucwords(strtolower($usuario->nombre)) . " " . ucwords(strtolower($usuario->apellido)); ?></br>
+        <div class="comparacion_grupo"></div>
+        <p>El siguiente gr&aacutefico muestra el ranking general del curso, despu&eacutes de realizada la actividad <?php echo $nombre_actividad; ?>:</p>
+        <div id="ranking_curso"></div>
         <hr/>
-        <!--Div that will hold the pie chart-->
-        <b>Matriz de desempe&ntilde;o</b>
-        </br>
-        <style type="text/css" media="screen">
-            table { border: 1px solid black;float:left;width:148px;}
-            #matriz_desempeno{width:800px;margin:0;overflow:hidden;}
-        </style>
-        <div id="matriz_desempeno">
-            <?php
-            /*foreach($matriz_desempeno as $quiz => $columna) {
-                $celdas = '';
-                $logro_quiz = 0;
-                $total_preguntas = 0;
-                foreach ($columna as $celda) {
-                    $celdas .= '<tr>';
-                    if ($celda['logro'] < 60) {
-                        $celdas .= '<td class="reprueba">' . $celda['contenido']->nombre . '(' . $celda['logro'] . '%)</td>';
-                    } else {
-                        $celdas .= '<td>' . $celda['contenido']->nombre . '(' . $celda['logro'] . '%)</td>';
-                    }
-                    $celdas .= '</td>';
-                    $logro_quiz += $celda['logro'] * $celda['numero_preguntas'];
-                    $total_preguntas += $celda['numero_preguntas'];
+        <div id="promedio_grupo"></div>
+        <p>El promedio del curso fue <?php echo $promedio_grupo; ?> considerando solo a los estudiantes que rindieron la actividad.</p>
+        <hr/>
+        <div id="histograma"></div>
+        <p>El eje vertical representa la frecuencia, mientras que el eje horizontal intervalos de las notas.</p>
+        <hr/>
+        <div id="aprobados"></div>
+        <p>Del total de estudiantes, el <?php echo intval(($aprobados/($aprobados+$reprobados+$ausentes))*100); ?>% aprob&oacute y el <?php echo intval(($reprobados/($aprobados+$reprobados+$ausentes))*100); ?>% reprob&oacute,
+            mientras que un <?php echo intval(($ausentes/($aprobados+$reprobados+$ausentes))*100); ?>% no realiz&oacute la actividad y, por lo tanto,
+            reprob&oacute.
+        </p>
+        <hr/>
+        <div id="tabla_notas"></div>
+    <script>
+        <?php
+            $s='<table border=\"1\"><tr><th><b>Nombre</b></th><th><b>Nota '.$nombre_actividad.'</b></th></tr>';
+            foreach($notas_grupo as $nota){
+                $s=$s.'<tr><td>'.$nota->apellido.', '.$nota->nombre.'</td><td>';
+                if($nota->logro!=NULL){
+                    $s=$s.$nota->logro;
+                }else{
+                    $s=$s.'No Rinde';
                 }
-
-                echo '<table>';
-                echo '<tr><td><b>';
-                echo $quiz . '(' . round($logro_quiz / $total_preguntas) . '%)';
-                echo '</b></td></td>';
-                echo $celdas;
-                echo '</table>';
+                $s=$s.'</td></tr>';
             }
-            */?>
-        </div>
-        <hr/>
-        <br/>
-        <hr/>
-        <div class="comparacion_grupo">
-            <p>Según lo que respondiste en la actividad <?php echo $nombre_actividad; ?>, cerrada el día <?php echo $fecha_cierre; ?>, tus resultados son los siguientes:</p>
-            <div id="ranking_curso"></div>
-            <hr/>
-            <div id="comparacion_grupo"></div>
-            <hr/>
-            <div id="histograma"></div>
-        </div>
+            $s=$s.'</table>';
+        ?>  
+        document.getElementById("tabla_notas").innerHTML= "<?php echo $s; ?>";
+        
+    </script>
     </body>
-</html>

f9893283a968567acf11077651a00f21e9e9a18b
Carlos Gajardo <cgajardo@litmon.com>
2012-02-07 11:33:31 -0300

    Ejemplo de vista interactiva para directores
    Aquí un ejemplo de la vista interactiva para directores. Se puede
    navegar forward, falta una navegación backward

--- controller/directorController.php
+++ controller/directorController.php
@@ -51,6 +51,47 @@ public function index() {
 }
 
 
+public function data(){
+	
+	$arbol_tiempo = array();
+	
+	if(isset($_GET['sede'])){
+		
+		$id_sede = $_GET['sede'];
+		$sede = DAOFactory::getSedesDAO()->load($id_sede);
+		$arbol_tiempo['nombre'] = $sede->nombre;
+		//comienza iteración
+		$cursos = DAOFactory::getCursosDAO()->getCursosInSede($sede->id);
+		$suma_cursos = 0;
+		//buscamos todos los cursos en una sede
+		foreach ($cursos as $curso){
+			$grupos = DAOFactory::getGruposDAO()->getGruposInCurso($curso->id);
+			$suma_grupos = 0;
+			//buscamos todos todos grupos en un curso
+			foreach ($grupos as $grupo){
+				$alumnos = DAOFactory::getPersonasDAO()->getEstudiantesInGroup($grupo->id);
+				$suma_alumnos = 0;
+				//buscamos todos los alumnos de un grupo (sumamos su tiempo)
+				foreach ($alumnos as $alumno){
+					$arbol_tiempo['detalle'][$curso->nombre]['detalle'][$grupo->nombre]['detalle'][$alumno->id]['nombre'] = $alumno->nombre.' '.$alumno->apellido;
+					//desde el inicio de los tiempos hasta hoy
+					$tiempo = DAOFactory::getLogsDAO()->getTiempoEntreFechas(0, time(), $alumno->id);
+					$arbol_tiempo['detalle'][$curso->nombre]['detalle'][$grupo->nombre]['detalle'][$alumno->id]['tiempo'] = $tiempo;
+					$suma_alumnos += $tiempo;
+				}
+				$arbol_tiempo['detalle'][$curso->nombre]['detalle'][$grupo->nombre]['tiempo'] = $suma_alumnos;
+				$suma_grupos += $suma_alumnos;
+			}
+			$arbol_tiempo['detalle'][$curso->nombre]['tiempo'] = $suma_grupos;
+			$suma_cursos += $suma_grupos;
+		}
+		$arbol_tiempo['tiempo'] = $suma_cursos;	
+	}//termina el if
+	
+	$this->registry->template->arbol = $arbol_tiempo;
+	$this->registry->template->show('director/json');
+}
+
 public function view(){
 	/*** should not have to call this here.... FIX ME ***/
 	$this->registry->template->blog_heading = 'This is the blog heading';
--- views/director/index.php
+++ views/director/index.php
@@ -2,6 +2,8 @@
  <html xmlns="http://www.w3.org/1999/xhtml">
    <head>
      <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+     <link rel="stylesheet" type="text/css" href="/reportes/views/styles/galyleo.css" />
+     <script type="text/javascript" src="/reportes/views/js/json.js"></script>
      <title>Informe para directores</title>
      <script type="php">
 	 <?php
@@ -28,7 +30,7 @@
 
         var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
         chart.draw(data, options);
-        google.visualization.events.addListener(chart, 'select', selectHandler);
+        google.visualization.events.addListener(chart, 'select', loadXMLDoc);
         function selectHandler() {
 			var selection = chart.getSelection();
         	var message = "";
@@ -43,14 +45,78 @@
         	    }
         	}
         	chart.clearChart();
-        	chart.draw(data, options);
-        	alert('You selected ' + message);
+        	//se sobreescribe el gráfico 
+        	chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
+
+        	data = new google.visualization.DataTable();
+            data.addColumn('string', 'Curso');
+            data.addColumn('number', 'Tiempo');
+            data.addRows([<?php echo $data_sedes;?>]);
+            options = {
+                    width: 800, height: 240,
+                    title: 'Tiempo de uso de la plataforma por Curso',
+                    hAxis: {title: 'Minutos', titleTextStyle: {color: 'blue'}}
+            };
+            
+            chart.draw(data, options);
+            google.visualization.events.addListener(chart, 'select', selectHandler);
         }
+
+        function loadXMLDoc(){
+        	var xmlhttp;
+        	var id = 0;
+        	var selection = chart.getSelection();
+        	for (var i = 0; i < selection.length; i++) {
+        		var item = selection[i]; 
+        	    if (item.row != null && item.column != null) {
+        	    	id = 3;
+        	    } else if (item.row != null) {
+        	    	id = 4;
+        	    } else if (item.column != null) {
+        	    	id = 3;
+        	    }
+        	}
+        	
+        	if (window.XMLHttpRequest){
+            	// code for IE7+, Firefox, Chrome, Opera, Safari
+          		xmlhttp=new XMLHttpRequest();
+          	}
+        	else{
+            	// code for IE6, IE5
+          		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+          	}
+        	xmlhttp.onreadystatechange=function() { 
+          		if (xmlhttp.readyState==4 && xmlhttp.status==200){
+          			document.getElementById("myDiv").innerHTML=xmlhttp.responseText;
+          			var j = JSON.parse(xmlhttp.responseText);
+          			chart.clearChart();
+                	//se sobreescribe el gráfico 
+                	chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
+
+                	data = new google.visualization.DataTable();
+                    data.addColumn('string', 'Curso');
+                    data.addColumn('number', 'Tiempo');
+                    data.addRows(j);
+                    options = {
+                            width: 800, height: 240,
+                            title: 'Tiempo de uso de la plataforma por Curso',
+                            hAxis: {title: 'Minutos', titleTextStyle: {color: 'blue'}}
+                    };
+                    
+                    chart.draw(data, options);
+                    google.visualization.events.addListener(chart, 'select', selectHandler);
+            	}
+          	}
+        	xmlhttp.open("GET","director/data?sede="+id,true);
+        	xmlhttp.send();
+        }
+        
       }
     </script>
      
    </head>
    <body style="font-family: Arial;border: 0 none;">
      <div id="chart_div" style="width: 700px; height: 400px;"></div>
+     <div id="myDiv"><h2>Let AJAX change this text</h2></div>
    </body>
  </html>

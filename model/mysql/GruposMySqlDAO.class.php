8101e959f09582743e21a90ddd98286081b260ac
Jorge Toro <maca@maca-PC>
2012-02-08 11:29:17 -0300

    eliminadas 2 funciones duplicadas

--- model/mysql/GruposMySqlDAO.class.php
+++ model/mysql/GruposMySqlDAO.class.php
@@ -66,43 +66,7 @@ class GruposMySqlDAO implements GruposDAO{
 		return $this->getRow($sqlQuery);
 	}
 	
-	
-	/**
-	 * Devuelve una lista de Grupos asociados a un Curso
-	 * 
-	 * @author cgajardo
-	 * @param int $curso_id
-	 */
-	public function getGruposInCurso($curso_id){
-		$sql = 'SELECT g.* '.
-			'FROM grupos as g, cursos_has_grupos as cg '.
-			'WHERE g.id = cg.id_grupo AND cg.id_curso = ? ';
-		
-		$sqlQuery = new SqlQuery($sql);
-		$sqlQuery->setNumber($curso_id);
-		
-		return $this->getList($sqlQuery);
-	}
-	
-	
-	/**
-	 * Esta función devuelve el grupo asociado a un curso y un estudiante
-	 * 
-	 * @author cgajardo
-	 * @param int $usuario_id
-	 * @param int $curso_id
-	 */
-	public function getGrupoByCursoAndUser($usuario_id, $curso_id){
-		$sql = 'SELECT g.* '.
-		'FROM grupos as g, grupos_has_estudiantes as ge, cursos_has_grupos as cg '.
-		'WHERE ge.id_grupo = g.id AND cg.id_grupo = g.id AND ge.id_persona = ? AND cg.id_curso = ? ';
-		
-		$sqlQuery = new SqlQuery($sql);
-		$sqlQuery->setNumber($usuario_id);
-		$sqlQuery->setNumber($curso_id);
-		
-		return $this->getRow($sqlQuery);
-	}
+
 	
 	
 	/**

837a4f6c3e4a4798a94861bf70b499f7e6de63b5
Carlos Gajardo <cgajardo@litmon.com>
2012-02-16 16:58:26 -0300

    Logro para el director
    Vista de logro macro para director

--- views/director/detalle_tiempo.php
+++ views/director/detalle_tiempo.php
@@ -1,7 +1,1 @@
-<?php
-$cadena = '[';
-foreach (array_reverse($tiempos_semanas) AS $fecha => $tiempo){
-	$cadena .= '["'.$fecha.'",'.round($tiempo/60,1).'],';
-}
-echo substr($cadena, 0, -1).']';
-?>
+matriz de desempeño

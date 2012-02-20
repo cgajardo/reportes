613d38bcd99f47163cc4dfab0a3c91d269491b11
Carlos Gajardo <cgajardo@litmon.com>
2012-02-20 18:47:54 -0300

    Codificaciones: You SUCK!
    Muchos problemas de codificación bien torpes

--- views/index.php
+++ views/index.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <html>
 <h1>Bienvenido</h1>
 <p>Prueba ingresando como:</p>
@@ -20,3 +21,27 @@
   <li><a href="./directores/index?id=8292&platform=utfsm">Rector Santa Maria</a></li>
 </ul>
 </html>
+=======
+<html>
+<h1>Bienvenido</h1>
+<p>Prueba ingresando como:</p>
+<h4>Alumnos:</h4>
+<ul>
+  <li><a href="./enrutador/index&params=lnFtbVjp2gmDCYidkYAPdhBOndpqcSXU">Andr&eacute;s Cort&eacute;s (Santiago)</a></li>
+  <li><a href="./enrutador/index&params=lnFtbVjp2gmDCYidkYAPdg5PRTyJOtPV">Lucia Luppichini (Santiago)</a></li>
+  <li><a href="./enrutador/index&params=lnFtbVjp2gmDCYidkYAPds6xYVFTKwZV">Nadia Tobar (Valparaiso)</a></li>
+  <li><a href="./enrutador/index&params=lnFtbVjp2gmDCYidkYAPdu78647p4IK8"> Samuel Norero (Valparaiso)</a></li>
+</ul>
+<br/>
+<h4>Profesores:</h4>
+<ul>
+
+  <li><a href="./enrutador/index?params=lnFtbVjp2gmDCYidkYAPdnF7J3QHE3yH">Leonelo Iturriaga (santiago)</a></li>
+  <li><a href="./enrutador/index?params=lnFtbVjp2gmDCYidkYAPdjJ5vJgbSEHw">Sigrid L&aacute;zaruz (valparaiso)</a></li>
+</ul>
+<h4>Rectores:</h4>
+<ul>
+  <li><a href="./directores/index?id=8292">Rector Santa Maria</a></li>
+</ul>
+</html>
+>>>>>>> 34539338c1daa791dd53afc8cbfe19ed66dcaa96

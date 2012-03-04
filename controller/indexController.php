6ee090f41d26f55f9bffeb1cd82f2c13b73f1301
Carlos Gajardo <cgajardo@litmon.com>
2012-03-04 19:24:20 -0300

    Release 1.2.1 no echos

--- controller/indexController.php
+++ controller/indexController.php
@@ -5,7 +5,7 @@ Class indexController Extends baseController {
 public function index() {
 	session_start();
 	session_destroy();
-	echo $this->encrypter->encode("platform=utfsm&username=cgajardo");
+	//echo $this->encrypter->encode("platform=utfsm&username=cgajardo");
 	/*** load the index template ***/
      $this->registry->template->show('index');
 }

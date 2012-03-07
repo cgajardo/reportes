6baf21896b2d8615b7668792ab2d7522de369bbc
Carlos Gajardo <cgajardo@litmon.com>
2012-03-07 10:56:49 -0300

    Pagina de inicio limpia
    ejemplos se mueves a ./index/showtest

--- controller/indexController.php
+++ controller/indexController.php
@@ -3,11 +3,15 @@
 Class indexController Extends baseController {
 
 public function index() {
+     $this->registry->template->show('index/bienvenido');
+}
+
+public function showtest(){
 	session_start();
 	session_destroy();
 	//echo $this->encrypter->encode("platform=utfsm&user=18459086");
 	/*** load the index template ***/
-     $this->registry->template->show('index');
+	$this->registry->template->show('index');
 }
 
 }

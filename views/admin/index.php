50e3c5ccdc6efc43b5b3a71c0ea0883588d1055a
JorgePaz <JorgePaz@JorgePaz-HP>
2012-05-09 09:55:14 -0400

    agregue una prueba para pagina de admin

---
+++ views/admin/index.php
@@ -0,0 +1,7 @@
+<html>
+<body>
+<a href=http://reportes.galyleo.net/instituciones>Instituciones</a></p>
+<a href=http://reportes.galyleo.net/sedes>Sedes</a></p>
+<a href=http://reportes.galyleo.net/contenidos>Contenidos</a></p>
+</body>
+</html>

0f328c8668e5cb45c127a95fafd16f510d6cf359
JorgePaz <JorgePaz@JorgePaz-HP>
2012-06-07 10:33:47 -0400

    Modificacion de pagina de administracion

--- views/admin/index.php
+++ views/admin/index.php
@@ -3,8 +3,8 @@
     <a href="<?php echo str_replace("index.php","",$_SERVER['PHP_SELF']);?>instituciones">Instituciones</a><br/>
     <a href="<?php echo str_replace("index.php","",$_SERVER['PHP_SELF']);?>sedes">Sedes</a><br/>
     <a href="<?php echo str_replace("index.php","",$_SERVER['PHP_SELF']);?>contenidos">Ver contenidos</a><br/>
-    <a href="<?php echo str_replace("index.php","",$_SERVER['PHP_SELF']);?>contenidos/agregar">--Nuevo Contenido</a><br/>
-    <a href="<?php echo str_replace("index.php","",$_SERVER['PHP_SELF']);?>contenidos/asociar_categorias">--Asociar Contenido a Categorias</a><br/>
-    <a href="<?php echo str_replace("index.php","",$_SERVER['PHP_SELF']);?>contenidos/asociar_preguntas">--Asociar Contenido a Preguntas</a><br/>
+    <a href="<?php echo str_replace("index.php","",$_SERVER['PHP_SELF']);?>contenidos/agregar">---Nuevo Contenido</a><br/>
+    <a href="<?php echo str_replace("index.php","",$_SERVER['PHP_SELF']);?>contenidos/asociar_categorias">---Asociar Contenido a Categorias</a><br/>
+    <a href="<?php echo str_replace("index.php","",$_SERVER['PHP_SELF']);?>contenidos/asociar_preguntas">---Asociar Contenido a Preguntas</a><br/>
 </body>
 </html>
